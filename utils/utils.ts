92b574ce146eaf337be3f53b80497708a9c690f9
fbourdenet <florian.bourdenet@gmail.com>
2023-04-21 11:58:54 +0200

    fixed capitalize function typo

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,5 @@
-const capitalize = (sentance: string) => {
-    return sentance.charAt(0).toUpperCase() + sentance.slice(1);
+const capitalize = (sentence: string) => {
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
 }
 
 export {
